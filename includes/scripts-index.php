a3bfb4b976ed8ef49f5260ea85862a36e53f6225
Olivier Coq <coq.olivier36@gmail.com>
2018-01-26 15:17:57 -0500

    took out tawk.to

--- includes/scripts-index.php
+++ includes/scripts-index.php
@@ -14,18 +14,6 @@
                 $(this).ekkoLightbox();
             });
         </script>
-            <!--Live Chat-->
-        <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/58b909f31919dc09f90e9091/default';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-            })();
-        </script>
 <!--End of Tawk.to Script-->
         <!--Masked Input-->
         <script src="/JS/jquery.maskedinput.js"></script>
